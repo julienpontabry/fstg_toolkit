07b2d6a549e71e7802a0f2e4577f6e6e84d42d3b
jpontabry <jpontabry@unistra.fr>
2024-10-18 09:26:53 +0200

    Update the doctests runner.

--- tests/doctests_runner.py
+++ tests/doctests_runner.py
@@ -1,4 +1,6 @@
 if __name__ == '__main__':
     import doctest
-    from fmri_st_graph import graph
-    doctest.testmod(graph)
+    from fmri_st_graph import graph, factory, io
+    doctest.testmod(graph, optionflags=doctest.NORMALIZE_WHITESPACE)
+    doctest.testmod(factory, optionflags=doctest.NORMALIZE_WHITESPACE)
+    doctest.testmod(io, optionflags=doctest.NORMALIZE_WHITESPACE)
