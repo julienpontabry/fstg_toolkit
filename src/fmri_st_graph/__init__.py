bd53aa6d8825c10a24a591173084545634d136d6
jpontabry <jpontabry@unistra.fr>
2024-10-18 12:30:00 +0200

    Update the package import.

---
+++ src/fmri_st_graph/__init__.py
@@ -0,0 +1,6 @@
+from .graph import SpatioTemporalGraph
+from .factory import spatio_temporal_graph_from_corr_matrices
+from .io import load_spatio_temporal_graph, save_spatio_temporal_graph
+from .simulation import CorrelationMatrixSequenceSimulator
+
+__version__ = 0.1
