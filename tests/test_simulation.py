2d6d8709a0a39784063fe4e6d45bf987dc2a5aa1
jpontabry <jpontabry@unistra.fr>
2024-10-22 17:15:55 +0200

    Add tests for patterns generation.

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -4,8 +4,10 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 
-from fmri_st_graph import SpatioTemporalGraph, load_spatio_temporal_graph
+from fmri_st_graph import SpatioTemporalGraph, load_spatio_temporal_graph, generate_pattern
+from fmri_st_graph.graph import RC5
 from fmri_st_graph.simulation import CorrelationMatrixSequenceSimulator
+
 
 class CorrelationMatrixSimulationTestCase(unittest.TestCase):
     @staticmethod
@@ -54,3 +56,31 @@ class CorrelationMatrixSimulationTestCase(unittest.TestCase):
 
         for i in range(4):
             self.assertEqual(1, matrix[i, i])
+
+
+class SpatioTemporalGraphSimulationTestCase(unittest.TestCase):
+    def test_generate_pattern(self):
+        pattern = generate_pattern(
+            networks_list=[[((1, 5), 1, -0.2), ((6, 7), 2, 0.3), ((8, 10), 2, 0.6)],
+                         [((1, 5), 1, 0.6), ((6, 10), 2, -0.5)]],
+            spatial_edges=[(1, 2, 0.45), (4, 5, 0.8)],
+            temporal_edges=[(1, 4, 'eq'), ((2, 3), 5, 'merge')])
+
+        self.assertEqual([1, 2, 3, 4, 5], list(pattern.graph.nodes))
+        self.assertEqual(dict(t=0, areas={1, 2, 3, 4, 5}, region='Region 1', internal_strength=-0.2),
+                         pattern.nodes[1])
+        self.assertEqual(dict(t=1, areas={6, 7, 8, 9, 10}, region='Region 2', internal_strength=-0.5),
+                         pattern.nodes[5])
+
+        self.assertEqual([(1, 2), (1, 4), (2, 1), (2, 5), (3, 5), (4, 5), (5, 4)], list(pattern.graph.edges))
+        self.assertEqual(dict(correlation=0.45, t=0, type='spatial'), pattern.graph.edges[1, 2])
+        self.assertEqual(dict(correlation=0.45, t=0, type='spatial'), pattern.graph.edges[2, 1])
+        self.assertEqual(dict(transition=RC5.PP, type='temporal'), pattern.graph.edges[2, 5])
+        self.assertEqual(dict(transition=RC5.PP, type='temporal'), pattern.graph.edges[3, 5])
+
+        id_list = list(range(1, 11))
+        expected = pd.DataFrame({'Id_Area': id_list,
+                                 'Name_Area': [f"Area {i}" for i in id_list],
+                                 'Name_Region': ["Region 1"]*5 + ["Region 2"]*5})
+        expected.set_index('Id_Area', inplace=True)
+        pd.testing.assert_frame_equal(expected, pattern.areas)
--- tests/tests_runner.py
+++ tests/tests_runner.py
@@ -1,11 +1,12 @@
 import unittest
 
-from test_simulation import CorrelationMatrixSimulationTestCase
+from test_simulation import CorrelationMatrixSimulationTestCase, SpatioTemporalGraphSimulationTestCase
 
 
 def simulation_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(CorrelationMatrixSimulationTestCase))
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(SpatioTemporalGraphSimulationTestCase))
     return suite
 
 if __name__ == '__main__':
