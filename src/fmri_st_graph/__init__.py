d9a3c61232b3717d732ff671664aa2ef7db1147d
jpontabry <jpontabry@unistra.fr>
2024-10-28 12:44:20 +0100

    Add visualization function to the package.

--- src/fmri_st_graph/__init__.py
+++ src/fmri_st_graph/__init__.py
@@ -2,5 +2,6 @@ from .graph import SpatioTemporalGraph
 from .factory import spatio_temporal_graph_from_corr_matrices
 from .io import load_spatio_temporal_graph, save_spatio_temporal_graph
 from .simulation import CorrelationMatrixSequenceSimulator, generate_pattern, SpatioTemporalGraphSimulator
+from .visualization import multipartite_plot, spatial_plot, temporal_plot
 
 __version__ = 0.1
