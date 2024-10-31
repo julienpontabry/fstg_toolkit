26b9dc7558d7e478d78fe5695de559025afd0693
jpontabry <jpontabry@unistra.fr>
2024-10-31 10:43:55 +0100

    Improve the plot profiling script.

--- tests/profile_plot.py
+++ tests/profile_plot.py
@@ -3,9 +3,12 @@ if __name__ == '__main__':
     import fmri_st_graph as fg
 
     graph = fg.load_spatio_temporal_graph('/tmp/st_graph.zip')
-    fg.spatial_plot(graph, 0)
+
+    timer = timeit.Timer(lambda: fg.spatial_plot(graph, 0))
+    n, _ = timer.autorange()
+    r = 5
+    print(f"Min. time for spatial plot is {round(min(timer.repeat(r, n)) * 1_000)}ms (over {r} repeats).")
 
     timer = timeit.Timer(lambda: fg.temporal_plot(graph))
     n, _ = timer.autorange()
-    r = 5
-    print(f"Min. time is {round(min(timer.repeat(r, n)) * 1_000)}ms (over {r} repeats).")
+    print(f"Min. time for temporal plot is {round(min(timer.repeat(r, n)) * 1_000)}ms (over {r} repeats).")
