6b83b2ed161da2ba84bd25ac5c49655074a5daba
jpontabry <jpontabry@unistra.fr>
2024-11-19 17:11:07 +0100

    Add CLI for patterns generation.

--- src/fmri_st_graph/__main__.py
+++ src/fmri_st_graph/__main__.py
@@ -1,10 +1,13 @@
+import re
 from pathlib import Path
+from typing import Optional
 
 import click
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+from fmri_st_graph import generate_pattern
 from .factory import spatio_temporal_graph_from_corr_matrices
 from .io import load_spatio_temporal_graph, save_spatio_temporal_graph
 from .visualization import spatial_plot, temporal_plot, multipartite_plot, dynamic_plot
@@ -16,7 +19,7 @@ def cli():
     pass
 
 
-# building
+## building ###################################################################
 
 @cli.command()
 @click.argument('correlation_matrices_path', type=click.Path(exists=True))
@@ -54,7 +57,7 @@ def build(correlation_matrices_path: str, areas_description_path: str, output_gr
             save_spatio_temporal_graph(graph, output)
 
 
-# plotting
+## plotting ###################################################################
 
 @click.group()
 @click.argument('graph_path', type=click.Path(exists=True))
@@ -127,11 +130,144 @@ def dynamic(ctx: click.core.Context, size: float, window: int):
     plt.show()
 
 
-# simulating
+## simulating #################################################################
+
+class GraphElementsDescription(click.ParamType):
+    main_desc: str = r'[-,:.\d]+'
+    elem_desc: str
+
+    def __init__(self):
+        self.main_regex = re.compile(self.main_desc)
+        self.elem_regex = re.compile(self.elem_desc)
+
+    def _convert_from_match(self, match: re.Match[str]) -> tuple[any,...]:
+        pass
+
+    def convert(self, value: any, param: Optional[click.Parameter], ctx: Optional[click.Context]) -> any:
+        elements = []
+
+        try:
+            for main_match in self.main_regex.finditer(value):
+                if not main_match:
+                    self.fail(f"{value!r} is not a valid spatial edge description!")
+
+                elem_match = self.elem_regex.match(main_match.group(0))
+
+                if not elem_match:
+                    self.fail(f"{value!r} is not a valid spatial edge description!")
+
+                elements.append(self._convert_from_match(elem_match))
+        except ValueError:
+            self.fail(f"{value!r} is not a valid spatial edge description!")
+
+        return elements
+
+
+class NetworksDescription(click.ParamType):
+    net_desc = re.compile(r'[-,:.\d]+')
+    reg_desc = re.compile(r'\s*(?P<range>\d+:\d+),(?P<id>\d+),(?P<strength>-?\d*.\d+)')
+
+    def __convert_network(self, value: str) -> list[tuple[tuple[int, int], int, float]]:
+        network = []
+
+        try:
+            for net_match in self.net_desc.finditer(value):
+                if not net_match:
+                    self.fail(f"{value!r} is not a valid network description!")
+
+                reg_match = self.reg_desc.match(net_match.group(0))
+
+                if not reg_match:
+                    self.fail(f"{value!r} is not a valid network description!")
+
+                tmp = reg_match.group('range').split(':')
+                areas = int(tmp[0]), int(tmp[1])
+                region = int(reg_match.group('id'))
+                internal_strength = float(reg_match.group('strength'))
+                network.append((areas, region, internal_strength))
+
+            return network
+        except ValueError:
+            self.fail(f"{value!r} is not a valid network description!")
+
+    def convert(self, value: any, param: Optional[click.Parameter], ctx: Optional[click.Context]) -> any:
+        if not isinstance(value, str):
+            self.fail(f"{value!r} is not a valid networks description!")
+
+        return [self.__convert_network(network_desc) for network_desc in value.split('/')]
+
+NETWORKS_DESCRIPTION = NetworksDescription()
+
+
+class SpatialEdgesDescription(GraphElementsDescription):#click.ParamType):
+    elem_desc = r'(?P<n1>\d+),(?P<n2>\d+),(?P<corr>-?\d*.\d+)'
+
+    def _convert_from_match(self, match: re.Match[str]) -> tuple[any,...]:
+        node1 = int(match.group('n1'))
+        node2 = int(match.group('n2'))
+        correlation = float(match.group('corr'))
+        return node1, node2, correlation
+
+SPATIAL_EDGES_DESCRIPTION = SpatialEdgesDescription()
+
+
+class TemporalEdgesDescription(GraphElementsDescription):
+    main_desc = r'[-,.\w]+'
+    elem_desc = r'(?P<n1>\d+(-\d+)?),(?P<n2>\d+),(?P<transition>\bsplit\b|\bmerge\b|\beq\b)'
+
+    @staticmethod
+    def __build_range(s: str) -> tuple[int, int] | int:
+        if '-' in s:
+            tmp = s.split('-')
+            return int(tmp[0]), int(tmp[1])
+        else:
+            return int(s)
+
+    def _convert_from_match(self, match: re.Match[str]) -> tuple[any,...]:
+        transition = match.group('transition')
+
+        if transition == 'eq':
+            node1 = int(match.group('n1'))
+            node2 = int(match.group('n2'))
+        else:
+            node1 = TemporalEdgesDescription.__build_range(match.group('n1'))
+            node2 = TemporalEdgesDescription.__build_range(match.group('n2'))
+
+        return node1, node2, transition
+
+TEMPORAL_EDGES_DESCRIPTION = TemporalEdgesDescription()
+
 
 @click.group()
 def simulate():
     """Simulate a spatio-temporal graph."""
+    pass
+
+@simulate.command()
+@click.argument('networks', type=NETWORKS_DESCRIPTION)
+@click.argument('spatial_edges', type=SPATIAL_EDGES_DESCRIPTION, required=False)
+@click.argument('temporal_edges', type=TEMPORAL_EDGES_DESCRIPTION, required=False)
+@click.option('-o', '--output_path', type=click.Path(writable=True), default="pattern.zip",
+              help="Path where to write the built graph.")
+def pattern(networks: list[list[tuple[tuple[int, int], int, float]]],
+            spatial_edges: list[tuple[int, int, float]] | None,
+            temporal_edges: list[tuple[int, int, str]] | None,
+            output_path: Path):
+    pattern = generate_pattern(networks_list=networks,
+                               spatial_edges=spatial_edges,
+                               temporal_edges=temporal_edges)
+    save_spatio_temporal_graph(pattern, output_path)
+
+
+@simulate.command()
+def graph():
+    # TODO input patterns + sequence
+    pass
+
+
+@simulate.command()
+def matrices():
+    # TODO input graph + areas + threshold
     pass
 
 
