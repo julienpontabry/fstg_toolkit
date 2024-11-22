c468e3f79d61bc0ff02d88a47d316a0d88d41cc9
jpontabry <jpontabry@unistra.fr>
2024-11-22 09:56:46 +0100

    Add CLI for simulation.

--- src/fmri_st_graph/__main__.py
+++ src/fmri_st_graph/__main__.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-from fmri_st_graph import generate_pattern
+from fmri_st_graph import generate_pattern, SpatioTemporalGraphSimulator, CorrelationMatrixSequenceSimulator
 from .factory import spatio_temporal_graph_from_corr_matrices
 from .io import load_spatio_temporal_graph, save_spatio_temporal_graph
 from .visualization import spatial_plot, temporal_plot, multipartite_plot, dynamic_plot
@@ -92,7 +92,7 @@ def multipartite(ctx: click.core.Context):
 
 @plot.command()
 @click.option('-t', '--time', type=click.IntRange(0), default=0,
-              help="The time index of the spatial subgraph to show.")
+              help="The time index of the spatial subgraph to show. Default is 0.")
 @click.pass_context
 def spatial(ctx: click.core.Context, time: int):
     """Plot as a spatial connectivity graph.
@@ -149,16 +149,16 @@ class GraphElementsDescription(click.ParamType):
         try:
             for main_match in self.main_regex.finditer(value):
                 if not main_match:
-                    self.fail(f"{value!r} is not a valid spatial edge description!")
+                    self.fail(f"{value!r} is not a valid description!")
 
                 elem_match = self.elem_regex.match(main_match.group(0))
 
                 if not elem_match:
-                    self.fail(f"{value!r} is not a valid spatial edge description!")
+                    self.fail(f"{value!r} is not a valid description!")
 
                 elements.append(self._convert_from_match(elem_match))
         except ValueError:
-            self.fail(f"{value!r} is not a valid spatial edge description!")
+            self.fail(f"{value!r} is not a valid description!")
 
         return elements
 
@@ -200,7 +200,8 @@ SPATIAL_EDGES_DESCRIPTION = SpatialEdgesDescription()
 
 class TemporalEdgesDescription(GraphElementsDescription):
     main_desc = r'[-,.\w]+'
-    elem_desc = r'(?P<n1>\d+(-\d+)?),(?P<n2>\d+),(?P<transition>\bsplit\b|\bmerge\b|\beq\b)'
+    elem_desc = r'(?P<n1>\d+(-\d+)?),(?P<n2>\d+(-\d+)?),(?P<transition>\bsplit\b|\bmerge\b|\beq\b)'
+    # TODO simplify patterns to: 1) n,m for eq, n,m-q for split and n-m,q for merge
 
     @staticmethod
     def __build_range(s: str) -> tuple[int, int] | int:
@@ -225,42 +226,73 @@ class TemporalEdgesDescription(GraphElementsDescription):
 TEMPORAL_EDGES_DESCRIPTION = TemporalEdgesDescription()
 
 
+class GraphSequenceDescription(click.ParamType):
+    def convert(self, value: any, param: Optional[click.Parameter], ctx: Optional[click.Context]) -> any:
+        if not isinstance(value, str):
+            self.fail(f"{value!r} is not a valid description!")
+
+        elements = value.split(' ')
+        sequence = []
+        for element in elements:
+            if element.isnumeric():
+                sequence.append(int(element))
+            elif element.lower().startswith('p') and element[1:].isnumeric():
+                sequence.append(element.lower())
+            else:
+                self.fail(f"{element!r} is not a valid description for a pattern or a spacing!")
+
+        return sequence
+
+
+GRAPH_SEQUENCE_DESCRIPTION = GraphSequenceDescription()
 
 
 @click.group()
-def simulate():
+@click.option('-o', '--output_path', type=click.Path(writable=True), default="output.zip",
+              help="Path where to write the simulated output.")
+@click.pass_context
+def simulate(ctx: click.core.Context, output_path: Path):
     """Simulate a spatio-temporal graph."""
-    pass
+    ctx.obj = output_path
 
 @simulate.command()
 @click.argument('networks', type=NETWORKS_DESCRIPTION)
 @click.argument('spatial_edges', type=SPATIAL_EDGES_DESCRIPTION, required=False)
 @click.argument('temporal_edges', type=TEMPORAL_EDGES_DESCRIPTION, required=False)
-@click.option('-o', '--output_path', type=click.Path(writable=True), default="pattern.zip",
-              help="Path where to write the built graph.")
-def pattern(networks: list[list[tuple[tuple[int, int], int, float]]],
+@click.pass_context
+def pattern(ctx: click.core.Context, networks: list[list[tuple[tuple[int, int], int, float]]],
             spatial_edges: list[tuple[int, int, float]] | None,
-            temporal_edges: list[tuple[int, int, str]] | None,
-            output_path: Path):
+            temporal_edges: list[tuple[int, int, str]] | None):
     """Generate a spatio-temporal graph pattern from description."""
     pat = generate_pattern(networks_list=networks,
                            spatial_edges=spatial_edges,
                            temporal_edges=temporal_edges)
-    save_spatio_temporal_graph(pat, output_path)
+    save_spatio_temporal_graph(pat, ctx.obj)
 
 
 @simulate.command()
-# @click.argument('sequence', type=)
-def graph():
-    """Generate a spatio-temporal graph from a combination of patterns."""
-    # TODO input patterns + sequence
-    pass
+@click.argument('patterns', nargs=-1, type=click.Path(exists=True))
+@click.argument('sequence_description', type=GRAPH_SEQUENCE_DESCRIPTION)
+@click.pass_context
+def sequence(ctx: click.core.Context, patterns: tuple[Path], sequence_description: list[str | int]):
+    """Generate a spatio-temporal graph from a sequence of patterns."""
+    pattern_graphs = {f'p{i+1}': load_spatio_temporal_graph(filepath)
+                      for i, filepath in enumerate(patterns)}
+    simulator = SpatioTemporalGraphSimulator(**pattern_graphs)
+    graph = simulator.simulate(*sequence_description)
+    save_spatio_temporal_graph(graph, ctx.obj)
 
 
 @simulate.command()
-def matrices():
-    # TODO input graph + areas + threshold
-    pass
+@click.argument('graph_path', type=click.Path(exists=True))
+@click.option('-t', '--threshold', type=click.FloatRange(0, 1, min_open=True, max_open=True),
+              default=0.4, help="The threshold of the correlation matrices. Default is 0.4.")
+@click.pass_context
+def correlations(ctx: click.core.Context, graph_path: Path, threshold: float):
+    graph = load_spatio_temporal_graph(graph_path)
+    simulator = CorrelationMatrixSequenceSimulator(graph, threshold=threshold)
+    matrices = simulator.simulate()
+    np.savez_compressed(ctx.obj, matrices)
 
 
 if __name__ == '__main__':
