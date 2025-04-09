e4ae9708eec27b0763cdcb5cc88d9474badf7a2b
Julien PONTABRY <jpontabry@unistra.fr>
2025-04-09 15:41:07 +0200

    Enabled other tabs when data has been loaded.

--- src/app/fstview.py
+++ src/app/fstview.py
@@ -1,6 +1,6 @@
 import plotly.io as pio
 from app.views import data, matrices, subject
-from dash_extensions.enrich import DashProxy, ServersideOutputTransform, dcc, html
+from dash_extensions.enrich import DashProxy, ServersideOutputTransform, dcc, callback, Input, set_props
 import dash_bootstrap_components as dbc
 
 # use orsjon to make JSON 5-10x faster
@@ -15,10 +15,10 @@ app.layout = dbc.Container(
     children=[
         # app's layout
         dbc.Tabs([
-            dbc.Tab(label="Data", children=data.layout),
-            dbc.Tab(label="Raw data view", children=matrices.layout),
-            dbc.Tab(label="Subject view", children=subject.layout, disabled=False),
-        ]),
+            dbc.Tab(label="Data", id='tab-data', tab_id='tab-data', children=data.layout),
+            dbc.Tab(label="Raw data view", id='tab-matrices', children=matrices.layout, disabled=True),
+            dbc.Tab(label="Subject view", id='tab-subject', children=subject.layout, disabled=True),
+        ], id='tabs'),
 
         # app's storage cache
         dcc.Store(id='store-desc', storage_type='session'),
@@ -26,6 +26,18 @@ app.layout = dbc.Container(
     ],
     fluid='xxl'
 )
+
+
+@callback(
+    Input('store-desc', 'data'),
+    Input('store-corr', 'data')
+)
+def set_tab_enabled(desc, corr):
+    if all(e is not None for e in (desc, corr)):
+        for tab in ('tab-matrices', 'tab-subject'):
+            set_props(tab, dict(disabled=False))
+        set_props('tabs', {'active_tab': 'tab-data'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
