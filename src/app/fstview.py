2e90cc4f9b48cc7ac3eae74b4954c0cb7c355604
Julien PONTABRY <jpontabry@unistra.fr>
2025-04-09 15:24:52 +0200

    Use dash bootstrap components.

--- src/app/fstview.py
+++ src/app/fstview.py
@@ -1,26 +1,31 @@
 import plotly.io as pio
 from app.views import data, matrices, subject
 from dash_extensions.enrich import DashProxy, ServersideOutputTransform, dcc, html
+import dash_bootstrap_components as dbc
 
 # use orsjon to make JSON 5-10x faster
 pio.json.config.default_engine = 'orjson'
 
 # app's definition
 app = DashProxy(title="fSTView - An fMRI spatio-temporal data viewer", name="fSTView",
-                transforms=[ServersideOutputTransform()])
+                transforms=[ServersideOutputTransform()],
+                external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div([
-    # app's layout
-    dcc.Tabs([
-        dcc.Tab(label="Data", children=data.layout),
-        dcc.Tab(label="Raw data view", children=matrices.layout),
-        dcc.Tab(label="Subject view", children=subject.layout),
-    ]),
+app.layout = dbc.Container(
+    children=[
+        # app's layout
+        dbc.Tabs([
+            dbc.Tab(label="Data", children=data.layout),
+            dbc.Tab(label="Raw data view", children=matrices.layout),
+            dbc.Tab(label="Subject view", children=subject.layout, disabled=False),
+        ]),
 
-    # app's storage cache
-    dcc.Store(id='store-desc', storage_type='session'),
-    dcc.Store(id='store-corr', storage_type='session'),
-])
+        # app's storage cache
+        dcc.Store(id='store-desc', storage_type='session'),
+        dcc.Store(id='store-corr', storage_type='session'),
+    ],
+    fluid='xxl'
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
