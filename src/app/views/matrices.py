7464ff59cade617daea36dac031a2ea42e71db02
Julien PONTABRY <jpontabry@unistra.fr>
2025-04-11 11:26:08 +0200

    Specify tooltip labels on the matrices graph.

--- src/app/views/matrices.py
+++ src/app/views/matrices.py
@@ -1,13 +1,13 @@
 from math import ceil
 
 from dash.exceptions import PreventUpdate
-from dash_extensions.enrich import Input, Output, callback, dcc
+from dash_extensions.enrich import Input, Output, State, callback, dcc
 import dash_bootstrap_components as dbc
 from plotly import graph_objects as go
 from plotly.subplots import make_subplots
 
 
-def build_matrices_figure(matrices, t, n_cols=5):
+def build_matrices_figure(matrices, t, desc, n_cols=5):
     # create figure for matrices to display
     names = list(matrices.keys())
     n = len(matrices)
@@ -19,10 +19,16 @@ def build_matrices_figure(matrices, t, n_cols=5):
         row = i // n_cols + 1
         col = i % n_cols + 1
         hm = go.Heatmap(
+            name=name,
+            x=desc['Name_Area'],
+            y=desc['Name_Area'],
             z=(matrices[name][t]),
             zmin=-1, zmax=1,
             coloraxis='coloraxis',
-            showscale=False
+            showscale=False,
+            hovertemplate='Row: %{y}'+
+                          '<br>Column: %{x}<br>'+
+                          'Corr: %{z:.2f}',
         )
         fig.add_trace(hm, row=row, col=col)
 
@@ -56,14 +62,15 @@ layout = [
     Output('mtx-slider-time', 'marks'),
     Input('store-corr', 'data'),
     Input('mtx-slider-time', 'value'),
+    State('store-desc', 'data'),
 )
-def update_figure(corr, slider_value):
+def update_figure(corr, slider_value, desc):
     if corr is None or len(corr) == 0:
         raise PreventUpdate
 
     max_slider_value = len(next(iter(corr.values()))) - 1
     marks_slider = {i: str(i) for i in range(0, max_slider_value + 1, max_slider_value//10)}
-    return build_matrices_figure(corr, slider_value), max_slider_value, marks_slider
+    return build_matrices_figure(corr, slider_value, desc), max_slider_value, marks_slider
 
 
 # TODO add a callback to resize everything when graph is zoomed
