642189b30bd3cabf9a243667881149f99d9bc0ff
Julien PONTABRY <jpontabry@unistra.fr>
2025-06-05 11:58:06 +0200

    Do not go back to data tab each tab the tabs are being refreshed.

--- src/app/fstview.py
+++ src/app/fstview.py
@@ -6,6 +6,8 @@ from dash import DiskcacheManager
 from dash_extensions.enrich import (
     DashProxy,
     Input,
+    Output,
+    State,
     ServersideOutputTransform,
     callback,
     dcc,
@@ -38,10 +40,14 @@ app.layout = dbc.Container(
         # app's layout
         dbc.Tabs([
             dbc.Tab(label="Data", id='tab-data', tab_id='tab-data', children=data.layout),
-            dbc.Tab(label="Raw data view", id='tab-matrices', children=matrices.layout, disabled=True),
-            dbc.Tab(label="Model", id='tab-model', children=model.layout, disabled=True),
-            dbc.Tab(label="Subject view", id='tab-subject', children=subject.layout, disabled=True),
-            dbc.Tab(label="Population view", id='tab-population', children=population.layout, disabled=True),
+            dbc.Tab(label="Raw data view", id='tab-matrices', tab_id='tab-matrices',
+                    children=matrices.layout, disabled=True),
+            dbc.Tab(label="Model", id='tab-model', tab_id='tab-model',
+                    children=model.layout, disabled=True),
+            dbc.Tab(label="Subject view", id='tab-subject', tab_id='tab-subject',
+                    children=subject.layout, disabled=True),
+            dbc.Tab(label="Population view", id='tab-population', tab_id='tab-population',
+                    children=population.layout, disabled=True),
         ], id='tabs'),
 
         # app's storage cache
@@ -62,22 +68,24 @@ app.layout = dbc.Container(
 @callback(
     Input('store-desc', 'data'),
     Input('store-corr', 'data'),
+    State('tabs', 'active_tab')
 )
-def set_tab_enabled(desc, corr):
+def set_tab_enabled_after_data(desc, corr, active_tab):
     if all(e is not None for e in (desc, corr)):
         set_props('tab-matrices', dict(disabled=False))
         set_props('tab-model', dict(disabled=False))
-        set_props('tabs', {'active_tab': 'tab-data'})  # FIXME find another way to update the display
+        set_props('tabs', {'active_tab': active_tab})
 
 
 @callback(
-    Input('store-graphs', 'data')
+    Input('store-graphs', 'data'),
+    State('tabs', 'active_tab')
 )
-def set_tab_enabled(graphs):
+def set_tab_enabled_after_model(graphs, active_tab):
     if graphs is not None:
         set_props('tab-subject', dict(disabled=False))
         set_props('tab-population', dict(disabled=False))
-        set_props('tabs', {'active_tab': 'tab-data'})  # FIXME find another way to update the display
+        set_props('tabs', {'active_tab': active_tab})
 
 
 if __name__ == '__main__':
