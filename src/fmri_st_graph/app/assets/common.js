325ebfa657edd910c4e6fdf28e6c86e8b9b30ac4
Julien PONTABRY <jpontabry@unistra.fr>
2025-07-18 10:31:29 +0200

    Fix some limit cases and add comments.

--- src/fmri_st_graph/app/assets/common.js
+++ src/fmri_st_graph/app/assets/common.js
@@ -1,10 +1,12 @@
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
         subject_node_hover: function(hoverData, storeHoverGraph) {
+            // check hover data input
             if (!hoverData || !hoverData.points || hoverData.points.length === 0 || !storeHoverGraph) {
                 return window.dash_clientside.no_update;
             }
         
+            // check for a graph figure with specified id
             const graphDiv = document.getElementById('st-graph');
             const figure = graphDiv.querySelector('.js-plotly-plot');
         
@@ -12,22 +14,28 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                 return window.dash_clientside.no_update;
             }
             
+            // check for any neighbouring coordinates associated with current point
             const point = hoverData.points[0];
             const x = point['x'];
             const y = point['y'];
-            
             const coord = storeHoverGraph[x][y];
-            // TODO check that coord is has two components
+
+            if (!coord || coord.length !== 2) {
+                return window.dash_clientside.no_update;
+            }
+            
+            // define properties of new points
             const xs = [x, ...coord[0]];
             const ys = [y, ...coord[1]];
-            
             const colors = Array(xs.length + 1).fill('green');
             colors[0] = 'red'; // Highlight the hovered point
             
+            // check last trace if it contains already some hovering points
             const n = figure.data.length;
             const trace = figure.data[n-1];
             
             if (!trace.name || trace.name !== 'hover-spatial-connections') {
+                // no trace found; creating a new one
                 Plotly.addTraces(figure, [{
                     x: xs, 
                     y: ys,
@@ -47,6 +55,7 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                     }
                 }]);        
             } else {
+                // found a trace; updating simply the points and colors
                 Plotly.restyle(figure, {
                         x: [xs],
                         y: [ys],
@@ -54,6 +63,7 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                     }, n-1);        
             }
             
+            // no update are needed in any case
             return window.dash_clientside.no_update;
         }
     }
