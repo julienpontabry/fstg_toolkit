9f1a9e5f367918b4f812914e036f08468d44d4f9
Julien PONTABRY <jpontabry@unistra.fr>
2025-07-29 11:21:17 +0200

    Improve type hinting.

--- src/fmri_st_graph/app/core/io.py
+++ src/fmri_st_graph/app/core/io.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, List, Dict, Set, Tuple
 from dataclasses import dataclass
 
 from pathlib import Path
@@ -16,10 +16,10 @@ class GraphsDataset:
     # TODO docstring
     loader: DataLoader
     areas_desc: pd.DataFrame
-    factors: list[set[str]]
+    factors: List[Set[str]]
     subjects: pd.DataFrame
 
-    def serialize(self) -> dict[str, Any]:
+    def serialize(self) -> Dict[str, Any]:
         return {
             'filepath': str(self.loader.filepath),
             'areas_desc': self.areas_desc.reset_index().to_dict('records'),
@@ -27,22 +27,22 @@ class GraphsDataset:
             'subjects': self.subjects.reset_index().to_dict('records')
         }
 
-    def __contains__(self, ids: tuple[str, ...]) -> bool:
+    def __contains__(self, ids: Tuple[str, ...]) -> bool:
         return ids in self.subjects.index
 
-    def get_graph(self, ids: tuple[str, ...]) -> SpatioTemporalGraph:
+    def get_graph(self, ids: Tuple[str, ...]) -> SpatioTemporalGraph:
         filename = self.subjects.loc[ids]['Graph']
         return self.loader.load_graph(self.areas_desc, filename)
 
     def has_matrices(self) -> bool:
         return 'Matrix' in self.subjects.columns
 
-    def get_matrix(self, ids: tuple[str, ...]) -> np.ndarray:
+    def get_matrix(self, ids: Tuple[str, ...]) -> np.ndarray:
         filename = self.subjects.loc[ids]['Matrix']
         return self.loader.load_matrix(filename)
 
     @staticmethod
-    def deserialize(data: dict[str, Any]) -> 'GraphsDataset':
+    def deserialize(data: Dict[str, Any]) -> 'GraphsDataset':
         filepath = data['filepath']
         n = len(data['factors'])
         subjects = pd.DataFrame(data['subjects'])
@@ -59,10 +59,12 @@ class GraphsDataset:
 
         if result is None:
             raise IOError("No dataset red.")
+        areas_desc: pd.DataFrame
         areas_desc, graphs_filenames, matrices_filenames = result
 
         # extract factors from filename (without extension
         filenames_without_ext = map(lambda name: name.split('.json')[0], graphs_filenames)
+        factors: List[Set[str]]
         factors, ids = split_factors_from_name(filenames_without_ext)
 
         # create a subject's table with factors as index and filenames as data
