f33b5df5d114bc3787981cbdb49404d20bbdcb98
Julien PONTABRY <jpontabry@unistra.fr>
2025-08-13 11:16:14 +0200

    Sort the factors values to have always the same order.

--- src/fmri_st_graph/app/views/common.py
+++ src/fmri_st_graph/app/views/common.py
@@ -6,14 +6,14 @@ import dash_bootstrap_components as dbc
 plotly_config = dict(displayModeBar='hover', displaylogo=False)
 
 
-def update_factor_controls(prefix: str, factors: list[set[str]], multi: bool = True):
+def update_factor_controls(prefix: str, factors: list[set[str]], multi: bool = True) -> list[dbc.Row]:
     if factors is None or len(factors) == 0:
         raise PreventUpdate
 
     controls = []
 
     for i, factor in enumerate(factors):
-        factor_values = list(factor)
+        factor_values = sorted(list(factor))
 
         if len(factor_values) > 0:
             value = factor_values if multi else factor_values[0]
