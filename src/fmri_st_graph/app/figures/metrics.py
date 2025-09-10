5f460093105167826aa974ddf4cbbe0a3744b404
Julien PONTABRY <jpontabry@unistra.fr>
2025-09-10 14:56:53 +0200

    Make the distribution comparison work.

--- src/fmri_st_graph/app/figures/metrics.py
+++ src/fmri_st_graph/app/figures/metrics.py
@@ -2,6 +2,7 @@ from typing import Type
 
 import pandas as pd
 import plotly.express as px
+from plotly import graph_objects as go
 
 
 def __check_object_types(series: pd.DataFrame, dtype: Type):
@@ -13,8 +14,8 @@ def build_metrics_plot(metrics: pd.DataFrame, selection: str):
 
     if isinstance(selected, pd.Series):
         return build_scalar_comparison_plot(selected)
-    elif __check_object_types(metrics[selection], list):
-        return build_distribution_comparison_plot(metrics, selection)
+    elif isinstance(selected, pd.DataFrame) and selected.columns.nlevels == 1:
+        return build_distribution_comparison_plot(selected)
     else:
         return {}
 
@@ -24,5 +25,22 @@ def build_scalar_comparison_plot(metric: pd.Series):
                      y=metric.name, box=True, points='all')
 
 
-def build_distribution_comparison_plot(metrics: pd.DataFrame, selection: str):
-    return px.bar(metrics, x="factor1", y=selection)
+def build_distribution_comparison_plot(metrics: pd.DataFrame):
+    factor = 'factor1'
+
+    percentages = metrics.divide(metrics.sum(axis='columns'), axis='index') * 100
+    values = percentages.groupby(factor).mean().T
+
+    fig = go.Figure(data=[
+        go.Bar(name=idx, x=values.columns, y=values.loc[idx].values)
+        for idx in values.index
+    ])
+
+    fig.update_layout(
+        barmode='stack',
+        yaxis_type='log',
+        xaxis_title=factor,
+        yaxis_title="Percentage",
+    )
+
+    return fig
