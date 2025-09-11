b6e06a88094798b5e4ad4964f27b9a20ed0d9a58
Julien PONTABRY <jpontabry@unistra.fr>
2025-09-11 17:13:33 +0200

    Make a coherent version number.

--- src/fstg_toolkit/__init__.py
+++ src/fstg_toolkit/__init__.py
@@ -4,4 +4,4 @@ from .io import load_spatio_temporal_graph, save_spatio_temporal_graph
 from .simulation import CorrelationMatrixSequenceSimulator, generate_pattern, SpatioTemporalGraphSimulator
 from .visualization import multipartite_plot, spatial_plot, temporal_plot
 
-__version__ = 0.1
+__version__ = '0.8.0'
