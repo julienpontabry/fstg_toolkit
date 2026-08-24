d6e494b1684e6cde50764cd3273356b86534a981
Julien PONTABRY <jpontabry@unistra.fr>
2026-08-24 12:00:33 +0200

    fix: wrong path to spminer.

--- src/fstg_toolkit/frequent/spminer.py
+++ src/fstg_toolkit/frequent/spminer.py
@@ -74,7 +74,7 @@ class SPMinerService:
         if self.__docker_image is None:
             # TODO use an external config file?
             tag = 'spminer:latest'
-            build_path = Path(__file__).parent.parent / 'spminer'
+            build_path = Path(__file__).parent.parent.parent / 'spminer'
             self.__docker_image = self.__docker_helper.load_local_image(tag, build_path)
 
     def run(self, input_dir: Path, output_dir: Path):
